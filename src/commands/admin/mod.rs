fa14269bb4a052fddb437d93f8ffad47ef728226
hebu <2rwin_rommel@naver.com>
2026-04-04 13:16:05 +0900

    chore: delete old bot logic

--- src/commands/admin/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod config;
-mod embeds;
-
-use serenity::all::CreateCommand;
-
-/// Returns all admin slash command definitions
-pub fn all() -> Vec<CreateCommand> {
-    vec![config::register()]
-}
